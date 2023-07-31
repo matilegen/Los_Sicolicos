13811234dd0494124d0af1e1e41ffbcf8ebd8551
matilegen <matiasalejandroserantes@hotmail.com>
2023-07-31 20:32:55 -0300

    puntos de recorridos del guardia

---
+++ Assets/Script/SistemaRecorrido.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SistemaRecorrido : MonoBehaviour
+{
+    public SistemaRecorrido puntoInicial;
+    public SistemaRecorrido PuntoFinal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/Sistema_Policia.cs
+++ Assets/Script/Sistema_Policia.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Sistema_Policia : MonoBehaviour
 {
+    GameObject ubicacion;
+    public SistemaRecorrido objetoDestino;
+    public Sistema_Armas arma=null;
+    public Sistema_Chaleco chaleco=null;
+    private double vidaPlayer; 
+    public double valorTotalObjetosRobados=0;
+    int NumeroRecorrido=0;
+    string texto;
+    int velocidadDeMovimiento=1;
+
+
+    //funcion
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +27,14 @@ public class Sistema_Policia : MonoBehaviour
     void Update()
     {
         
+        if(transform.position!=objetoDestino.transform.position){
+            this.transform.position = Vector3.MoveTowards(transform.position,objetoDestino.transform.position,velocidadDeMovimiento*Time.deltaTime);
+        }else{
+            objetoDestino=objetoDestino.PuntoFinal;
+        }
+        //Movimiento automatico
+        velocidadDeMovimiento=5;
+        
+
     }
 }
