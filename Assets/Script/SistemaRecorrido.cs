afed7a30341acc3b8fced31bcf2a08cf5455e1e0
matilegen <matiasalejandroserantes@hotmail.com>
2023-07-31 21:02:09 -0300

    agrego una pared

--- Assets/Script/SistemaRecorrido.cs
+++ Assets/Script/SistemaRecorrido.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SistemaRecorrido : MonoBehaviour
 {
-    public SistemaRecorrido puntoInicial;
     public SistemaRecorrido PuntoFinal;
     // Start is called before the first frame update
     void Start()
